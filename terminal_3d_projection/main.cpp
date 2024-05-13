cf2b63d81878c3c061e3af28608bf69786932fc2
yjwbcxy <yjwbcxy@protonmail.com>
2024-05-13 19:51:04 +0200

    some parametrization

--- terminal_3d_projection/main.cpp
+++ terminal_3d_projection/main.cpp
@@ -9,16 +9,22 @@
 #define RX 80
 #define RY 23
 
+#define R1 1
+#define R2 2
+
+#define K1 125
+#define K2 R1 + R2 * 2
+
 void ascii_frame() {
-    double A = 1, B = 1;
+    double rotation_x = 0, rotation_z = 0;
 
     while (true) {
         std::string print_buffer;
         std::vector<double> z_buffer;
-        A += 0.07;
-        B += 0.03;
-        double cos_A = cos(A), sin_A = sin(A);
-        double cos_B = cos(B), sin_B = sin(B);
+        rotation_x += 0.07;
+        rotation_z += 0.03;
+        double cos_rx = cos(rotation_x), sin_rx = sin(rotation_x);
+        double cos_rz = cos(rotation_z), sin_rz = sin(rotation_z);
 
         for (int i = 0; i < RX * RY; i++) {
             if (i % RX == RX - 1) {
@@ -33,25 +39,31 @@ void ascii_frame() {
             double cos_theta = cos(theta), sin_theta = sin(theta);
             for (double phi = 0; phi < 2 * M_PI; phi += 0.02) {
                 double cos_phi = cos(phi), sin_phi = sin(phi);
-                double h = cos_theta + 2;
-                double D = 1 / (sin_phi * h * sin_A + sin_theta * cos_A + 5);
-                double t = sin_phi * h * cos_A - sin_theta * sin_A;
+                double circle_x = cos_theta * R1 + R2,
+                       circle_y = sin_theta * R1;
+                double inverse_z = 1 / (sin_phi * circle_x * sin_rx +
+                                        circle_y * cos_rx + K2); //  1/z
+                double t = sin_phi * circle_x * cos_rx - sin_theta * sin_rx;
 
-                int x = (int)(40 + 30 * D * (cos_phi * h * cos_B - t * sin_B)),
-                    y = (int)(12 + 15 * D * (cos_phi * h * sin_B + t * cos_B));
+                int x = (int)(RX / 2 +
+                              K1 * inverse_z *
+                                  (cos_phi * circle_x * cos_rz - t * sin_rz)),
+                    y = (int)(RY / 2 +
+                              (K1 / 2) * inverse_z *
+                                  (cos_phi * circle_x * sin_rz + t * cos_rz));
                 double o = x + RX * y;
-                int N =
+                int normal =
                     (int)(8 *
-                          ((sin_theta * sin_A - sin_phi * cos_theta * cos_A) *
-                               cos_B -
-                           sin_phi * cos_theta * sin_A - sin_theta * cos_A -
-                           cos_phi * cos_theta * sin_B));
+                          ((sin_theta * sin_rx - sin_phi * cos_theta * cos_rx) *
+                               cos_rz -
+                           sin_phi * cos_theta * sin_rx - sin_theta * cos_rx -
+                           cos_phi * cos_theta * sin_rz));
                 if (y < RY && y >= 0 && x >= 0 && x <= RX - 1 &&
-                    D > z_buffer[o]) {
-                    z_buffer[o] = D;
+                    inverse_z > z_buffer[o]) {
+                    z_buffer[o] = inverse_z;
                     char _char;
-                    if (N > 0) {
-                        _char = ".,-~:;=!*#$@"[N];
+                    if (normal > 0) {
+                        _char = ".,-~:;=!*#$@"[normal];
                     } else {
                         _char = '.';
                     }
